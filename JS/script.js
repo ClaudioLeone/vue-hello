80fdf7e0dbc798db7afd7f3e0eed932d9fb2b4c6
ClaudioLeone <claleone92@gmail.com>
2023-03-28 17:51:58 +0200

    Completed exercise + BONUS

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,11 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
     data() {
-        return{
-            
+        return {
+            greet: "Hello Vue!",
+            imgSrc: "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*_k_VPpZfwmeTuIeUMgQQJA.png",
+            imgAlt: "Vue-3-0"
         }
     }
 }).mount('#app')
